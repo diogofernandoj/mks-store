b25b5af4e44c1752e99ac09046d9a3c61347caf4
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-29 22:29:37 -0300

    chore: add initial styled components setup

---
+++ app/styles/GlobalStyle.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #F9F9F9;
+  }
+`;
+
+export default GlobalStyle;
---
+++ app/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    primary: "#0F52BA",
+    grayLight: "#F9F9F9F9",
+  },
+};
+
+export default theme;
