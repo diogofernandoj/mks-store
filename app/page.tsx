bd4f80fbe60b96f433347ddfa62efe93197a80b3
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-30 17:51:19 -0300

    chore: add initial redux setup

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,22 @@ import Navbar from "./_components/Navbar";
 import Footer from "./_components/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Products from "./_components/Products";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 const Home = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Navbar />
-        <Products />
-        <Footer />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Navbar />
+          <Products />
+          <Footer />
+        </ThemeProvider>
+      </Provider>
     </QueryClientProvider>
   );
 };
---
+++ app/store/cartSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IProduct } from "../_lib/types";
+
+export interface ICartItem extends IProduct {
+  quantity: number;
+}
+
+interface CartState {
+  items: ICartItem[];
+}
+
+const initialState: CartState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addProduct: (state, action: PayloadAction<IProduct>) => {
+      const existingProduct = state.items.find(
+        (item) => item.id === action.payload.id
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    incrementQuantity: (state, action: PayloadAction<number>) => {
+      const product = state.items.find((item) => item.id === action.payload);
+      if (product) {
+        product.quantity += 1;
+      }
+    },
+    decrementQuantity: (state, action: PayloadAction<number>) => {
+      const product = state.items.find((item) => item.id === action.payload);
+      if (product) {
+        if (product.quantity > 1) {
+          product.quantity -= 1;
+        } else {
+          state.items = state.items.filter(
+            (item) => item.id !== action.payload
+          );
+        }
+      }
+    },
+  },
+});
+
+export const { addProduct, incrementQuantity, decrementQuantity } =
+  cartSlice.actions;
+export default cartSlice.reducer;
