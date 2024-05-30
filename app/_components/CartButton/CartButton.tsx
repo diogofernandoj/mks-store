4f789806bcaab0006f1f4596cc453357fb053946
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-30 19:00:31 -0300

    feat: ensure cart items is counted

--- app/_components/CartButton/CartButton.tsx
+++ app/_components/CartButton/CartButton.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import { CartButtonContainer, CartQuantity } from "./CartButton.styled";
+import { useSelector } from "react-redux";
+import { RootState } from "@/app/store/store";
 
 const CartButton = () => {
+  const itemsCount = useSelector((state: RootState) =>
+    state.cart.items.reduce((acc, curr) => acc + curr.quantity, 0)
+  );
+
   return (
     <CartButtonContainer>
       <Image src="/cart.png" width={19} height={18} alt="Cart Icon" />
-      <CartQuantity>0</CartQuantity>
+      <CartQuantity>{itemsCount}</CartQuantity>
     </CartButtonContainer>
   );
 };
