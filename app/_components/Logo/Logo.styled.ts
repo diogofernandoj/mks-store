cabfe27eabe2c2ce4664d9852633afe71b0fad45
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-29 22:56:06 -0300

    feat: add logo component

---
+++ app/_components/Logo/Logo.styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  width: fit-content;
+`;
+
+export const BoldTitle = styled.p`
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 19px;
+  color: #fff;
+
+  padding: 0;
+  margin: 0;
+`;
+
+export const SecondaryTitle = styled.p`
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 19px;
+  color: #fff;
+`;
---
+++ app/_components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import { BoldTitle, LogoContainer, SecondaryTitle } from "./Logo.styled";
+
+const Logo = () => {
+  return (
+    <LogoContainer>
+      <BoldTitle>MKS</BoldTitle>
+      <SecondaryTitle>Sistemas</SecondaryTitle>
+    </LogoContainer>
+  );
+};
+
+export default Logo;
