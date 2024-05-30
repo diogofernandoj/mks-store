7984d8d75a3c0173afc6cf066d104eb95894380d
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-30 12:41:21 -0300

    feat: add Product interface

---
+++ app/_lib/types.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  price: string;
+  photo: string;
+  createdAt: string;
+  updatedAt: string;
+}
