3a2dd9e74dd823780112d95eb87ce51f9be7bca4
Diogo Jorge <diogofernandoj@gmail.com>
2024-05-31 20:41:42 -0300

    test: ensure Footer component is rendered with correct text

---
+++ __tests__/Footer.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Footer from "../app/_components/Footer";
+
+describe("Footer component", () => {
+  it("should render the Footer component with correct text", () => {
+    render(<Footer />);
+
+    expect(
+      screen.getByText("MKS sistemas © Todos os direitos reservados")
+    ).toBeInTheDocument();
+  });
+});
