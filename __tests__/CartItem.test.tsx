47803cfb468931c3844c602c5db01eaa11172832
Diogo Jorge <diogofernandoj@gmail.com>
2024-06-01 13:03:22 -0300

    test: ensure CartItem component is rendered with correct item details

---
+++ __tests__/CartItem.test.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { useDispatch } from "react-redux";
+import CartItem from "../app/_components/CartItem";
+import {
+  ICartItem,
+  removeProductFromCart,
+  decrementQuantity,
+  incrementQuantity,
+} from "../app/store/cartSlice";
+
+jest.mock("react-redux", () => ({
+  useDispatch: jest.fn(),
+}));
+
+jest.mock("../app/store/cartSlice", () => ({
+  removeProductFromCart: jest.fn(),
+  decrementQuantity: jest.fn(),
+  incrementQuantity: jest.fn(),
+}));
+
+jest.mock("next/image", () => {
+  const MockImage = (props: any) => <img {...props} />;
+  MockImage.displayName = "MockImage";
+  return MockImage;
+});
+
+const mockItem: ICartItem = {
+  id: 1,
+  name: "Item 1",
+  brand: "Sample Brand",
+  description: "Sample Description",
+  price: "10",
+  quantity: 2,
+  photo: "Image URL",
+  createdAt: "2024-01-01",
+  updatedAt: "2024-01-01",
+};
+
+describe("CartItem component", () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    (useDispatch as any).mockReturnValue(dispatch);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render the CartItem component with correct item details", () => {
+    render(<CartItem item={mockItem} />);
+
+    expect(screen.getByText("Item 1")).toBeInTheDocument();
+    expect(screen.getByText("Qtd:")).toBeInTheDocument();
+    expect(screen.getByText("2")).toBeInTheDocument();
+    expect(screen.getByText("R$20")).toBeInTheDocument();
+    expect(screen.getByAltText("Item 1")).toBeInTheDocument();
+  });
+});
